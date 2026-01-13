4e5e45fde293cff2492a3b5f5cf91e4df02e5293
Vale77Bon <valentinhm071@gmail.com>
2026-01-12 20:08:46 -0600

    Forzando actualizacion v4 y arreglando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'; // 1. IMPORTANTE: Para que funcionen los títulos SEO
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,26 +10,36 @@ import SearchResults from './pages/SearchResults';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        {/* El Navbar siempre visible */}
-        <Navbar />
-        
-        {/* El contenido cambia según la ruta */}
-        <main className="flex-grow container mx-auto p-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/buscar" element={<ProfessorList />} />
-            <Route path="/profesor/:id" element={<ProfessorDetail />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/buscar" element={<SearchResults />} />
-          </Routes>
-        </main>
+    <HelmetProvider> {/* 2. Envolvemos toda la app para el SEO */}
+      <Router>
+        <div className="flex flex-col min-h-screen font-sans text-gray-900 bg-gray-50">
+          
+          {/* 3. LÍNEA MÁGICA: Esto forzará a Vercel a actualizarse sí o sí */}
+          <div className="bg-black text-white text-xs text-center py-1 font-bold">
+            VERSION 4.0 - ACTUALIZACIÓN FORZADA
+          </div>
 
-        {/* El Footer siempre visible */}
-        <Footer />
-      </div>
-    </Router>
+          <Navbar />
+          
+          <main className="flex-grow container mx-auto p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              
+              {/* CORRECCIÓN: Tenías dos rutas "/buscar". Dejamos SearchResults para la lupa */}
+              <Route path="/buscar" element={<SearchResults />} />
+              
+              {/* Si quieres usar ProfessorList, ponle otra ruta como "/lista" */}
+              <Route path="/lista" element={<ProfessorList />} /> 
+
+              <Route path="/profesor/:id" element={<ProfessorDetail />} />
+              <Route path="/admin" element={<Admin />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
 
